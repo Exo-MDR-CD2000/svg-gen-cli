e45d1b676fc4ab5c158b01a6f1c1cad4b3809687
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-08-23 19:13:26 -0700

    added index code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // main file where to start the server
+
+const CLI = require('./lib/cli'); // this should bring in the CLI class from cli.js
+
+const cli = new CLI(); // this should create a new instance of the CLI class
+
+cli.generate(); // this should call the generate method on the cli instance
